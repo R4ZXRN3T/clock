6b07add0aa74f4e549f810680a8408349c8faf05
Arian Pieroni <rizelloyd@gmail.com>
2026-02-16 18:13:08 +0100

    Added build script, optimizing the binary for size

---
+++ build.sh
@@ -0,0 +1,16 @@
+rm -rf "./final/clock-0.1.0-Linux-x86_64"
+
+rustup component add rust-src --toolchain nightly
+
+RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size" --release
+
+mkdir -p "./final/clock-0.1.0-Linux-x86_64"
+mv "./target/release/clock" "./final/clock-0.1.0-Linux-x86_64/clock"
+cd "./final/clock-0.1.0-Linux-x86_64"
+chmod +x clock
+upx --ultra-brute clock
+upx -t clock
+
+cd "../.."
+
+rm -rf "./target/"
